3fb7c5085462d3d3c8bb9a35989d22da5db9cd19
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2023-05-29 10:57:47 -0400

    added Controller for displaying Movie List

---
+++ MDEV1004-M2023-ICE3/Server/Controllers/movie.ts
@@ -0,0 +1,16 @@
+import { Request, Response, NextFunction } from 'express';
+
+import Movie from '../Models/movie';
+
+export function DisplayMovieList(req: Request, res: Response, next: NextFunction): void
+{
+    Movie.find({})
+    .then(function(data)
+    {
+        res.json(data);
+    })
+    .catch(function(err)
+    {
+        console.error(err);
+    });
+}
--- MDEV1004-M2023-ICE3/Server/Routes/index.js
+++ MDEV1004-M2023-ICE3/Server/Routes/index.js
@@ -5,11 +5,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 let router = express_1.default.Router();
+const movie_1 = require("../Controllers/movie");
 router.get('/', function (req, res, next) {
     res.render('index', { title: 'Express' });
 });
 router.get('/movie-list', function (req, res, next) {
-    res.json({});
+    (0, movie_1.DisplayMovieList)(req, res, next);
 });
 exports.default = router;
 //# sourceMappingURL=index.js.map
