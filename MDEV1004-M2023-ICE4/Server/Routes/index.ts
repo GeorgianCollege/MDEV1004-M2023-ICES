86725776fc8beb5ceb748969a009b64aba9250ce
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2023-06-05 08:06:16 -0400

    added project files for ICE4

---
+++ MDEV1004-M2023-ICE4/Server/Routes/index.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+let router = express.Router();
+
+/* Get the movie Controller */
+import { DisplayMovieList } from '../Controllers/movie';
+
+/* GET home page. */
+router.get('/', function(req, res, next) 
+{
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/movie-list', function(req, res, next)
+{
+  DisplayMovieList(req, res, next);
+});
+
+export default router;
