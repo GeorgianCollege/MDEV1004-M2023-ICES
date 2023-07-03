828df60a284aabec9ec1ddc95df79c7797ca3f4d
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2023-07-03 08:22:50 -0400

    added project files for ICE6

---
+++ MDEV1004-M2023-ICE6/Server/Config/app.ts
@@ -0,0 +1,61 @@
+// modules for express server
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+
+// modules for authentication
+import session from 'express-session';
+import passport from 'passport';
+import passportLocal from 'passport-local';
+
+// authentication objects
+let strategy = passportLocal.Strategy; // alias
+import User from '../Models/user';
+
+// Database modules
+import mongoose from 'mongoose';
+import db from './db';
+
+mongoose.connect(db.remoteURI);
+
+// DB Connection Events
+mongoose.connection.on('connected', () =>{
+    console.log(`Connected to MongoDB`);
+});
+
+mongoose.connection.on('disconnected', () =>{
+    console.log('Disconnected from MongoDB');
+});
+
+import indexRouter from '../Routes/index';
+
+let app = express();
+
+// middleware modules
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// setup express session
+app.use(session({
+    secret: db.secret,
+    saveUninitialized: false,
+    resave: false
+  }));
+
+// initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// implement an Auth Strategy
+passport.use(User.createStrategy());
+
+// serialize and deserialize user data
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use('/api/', indexRouter);
+
+export default app;
